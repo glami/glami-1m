0f332b1a94fb3f5bdb0c6e31759322533541cbe9
Vaclav Kosar <admin@vaclavkosar.com>
2022-09-05 20:55:15 +0200

    evaluate visual clip embeddings

--- eval_visual_clip.py
+++ eval_visual_clip.py
@@ -9,10 +9,8 @@ from load_dataset import (
     COL_NAME_LABEL_SOURCE, COL_NAME_CAT_NAME, CLIP_VISUAL_EMBS_DIR, COL_NAME_ITEM_ID, COL_NAME_NAME,
     COL_NAME_DESCRIPTION, COL_NAME_IMAGE_FILE,
 )
-from embracenet_utils import EmbraceConfig, load_embracenet, EmbraceNetTrimodalClassifier, EmbraceDataset
 from utils import calc_accuracy, chunker
 import torch
-from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
 
@@ -25,25 +23,26 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device)
 
+    print("Generating target embeddings from prompts...")
     category_name_to_prompt = dict()
     category_name_to_idx = dict()
     category_idx_to_name = dict()
     cat_embeddings = []
     for cat_i, cat_name in enumerate(df[COL_NAME_CAT_NAME].unique()):
+        # TODO men's!
+        # TODO prompt = "a photo of a " + cat_name.strip().replace('women-s', "women's").replace('womens', "women's").replace('-', ' ').replace(' and ', ' or ') + ", a type of fashion product"
         prompt = "a photo of a fashion product from " + cat_name.strip().replace('-', ' ').replace('womens', "women's").rstrip( 's') + " category" # TODO women-s
         # TODO consider multi-lingual for text eval although it should not matter in english
         prompt_tokens = clip.tokenize([prompt]).to(device)
         text_features = model.encode_text(prompt_tokens).cpu().detach().numpy()
-        category_name_to_prompt[cat_name] = cat_i
+        category_name_to_prompt[cat_name] = prompt
         category_idx_to_name[cat_i] = cat_name
         category_name_to_idx[cat_name] = cat_i
         text_features = text_features / np.linalg.norm(text_features)
         cat_embeddings.append(text_features)
 
     cat_embeddings = np.stack(cat_embeddings, axis=0).squeeze()
-
-    print("Binarizing targets...")
-
+    print(f'{category_name_to_prompt}')
 
     BATCH_SIZE = 256
     print("Evaluating...")
