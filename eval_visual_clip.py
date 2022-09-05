abb4fa0176a8c0411dab4b8b218eebe2b6e67ae1
Vaclav Kosar <admin@vaclavkosar.com>
2022-09-05 19:18:01 +0200

    evaluate visual clip embeddings

---
+++ eval_visual_clip.py
@@ -0,0 +1,69 @@
+import clip
+
+from load_dataset import (
+    download_dataset,
+    EXTRACT_DIR,
+    DATASET_URL,
+    get_dataframe,
+    COL_NAME_CATEGORY,
+    COL_NAME_LABEL_SOURCE, COL_NAME_CAT_NAME, CLIP_VISUAL_EMBS_DIR, COL_NAME_ITEM_ID, COL_NAME_NAME,
+    COL_NAME_DESCRIPTION, COL_NAME_IMAGE_FILE,
+)
+from embracenet_utils import EmbraceConfig, load_embracenet, EmbraceNetTrimodalClassifier, EmbraceDataset
+from utils import calc_accuracy, chunker
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import numpy as np
+
+
+if __name__ == "__main__":
+    download_dataset(EXTRACT_DIR, DATASET_URL)
+    dataset_dir = EXTRACT_DIR + "/glami-2022-dataset"
+    df = get_dataframe(dataset_dir, "test")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+
+    category_name_to_prompt = dict()
+    category_name_to_idx = dict()
+    category_idx_to_name = dict()
+    cat_embeddings = []
+    for cat_i, cat_name in enumerate(df[COL_NAME_CAT_NAME].unique()):
+        prompt = "a photo of a fashion product from " + cat_name.strip().replace('-', ' ').replace('womens', "women's").rstrip( 's') + " category" # TODO women-s
+        # TODO consider multi-lingual for text eval although it should not matter in english
+        prompt_tokens = clip.tokenize([prompt]).to(device)
+        text_features = model.encode_text(prompt_tokens).cpu().detach().numpy()
+        category_name_to_prompt[cat_name] = cat_i
+        category_idx_to_name[cat_i] = cat_name
+        category_name_to_idx[cat_name] = cat_i
+        text_features = text_features / np.linalg.norm(text_features)
+        cat_embeddings.append(text_features)
+
+    cat_embeddings = np.stack(cat_embeddings, axis=0).squeeze()
+
+    print("Binarizing targets...")
+
+
+    BATCH_SIZE = 256
+    print("Evaluating...")
+    predictions = []
+    targets = []
+    for batch in tqdm(
+            chunker(df.loc[:, [COL_NAME_ITEM_ID, COL_NAME_NAME, COL_NAME_DESCRIPTION, COL_NAME_IMAGE_FILE, COL_NAME_CAT_NAME]],
+                    BATCH_SIZE),
+            total=int(np.ceil(len(df) / BATCH_SIZE)),
+    ):
+        emb_files = [f"{CLIP_VISUAL_EMBS_DIR}/{x[0]}.npy" for x in batch.values]
+        embs_arr = [np.load(file) for file in emb_files]
+        embs_arr = [x.squeeze() / np.linalg.norm(x) for x in embs_arr]
+        embs = np.stack(embs_arr, axis=0)
+        similarities = embs @ cat_embeddings.transpose()
+        predictions.extend(similarities)
+
+        target_sr = batch[COL_NAME_CAT_NAME].map(category_name_to_idx)
+        targets.extend([np.eye(1, len(category_name_to_idx), i, dtype=int)[0].tolist() for i in target_sr])
+
+    accs = calc_accuracy(np.array(predictions), np.array(targets))
+    print(f"Accuracies:")
+    print(accs)
