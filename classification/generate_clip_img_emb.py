bd1e80a03b8e5b8cfdfa6d51d4952a36dff58421
Vaclav Kosar <admin@vaclavkosar.com>
2023-02-09 10:40:46 +0100

    generate image embeddings method

--- classification/generate_clip_img_emb.py
+++ classification/generate_clip_img_emb.py
@@ -1,5 +1,7 @@
 import os
 from glob import glob
+
+import pandas as pd
 from tqdm import tqdm
 from load_dataset import (
     EXTRACT_DIR,
@@ -19,11 +21,7 @@ import clip
 from PIL import Image
 
 
-if __name__ == "__main__":
-    download_dataset()
-    train_df = get_dataframe("train")
-    test_df = get_dataframe("test")
-
+def generate_image_embeddings(df: pd.DataFrame, df_name: str):
     if not os.path.exists(CLIP_VISUAL_EMBS_DIR):
         os.mkdir(CLIP_VISUAL_EMBS_DIR)
 
@@ -43,22 +41,28 @@ if __name__ == "__main__":
             image_features = model.encode_image(torch.concat(images, dim=0))
             return image_features.cpu().detach().numpy()
 
-
-    for df, df_name in zip([train_df, test_df], ["train", "test"]):
-        file_filter = df[COL_NAME_ITEM_ID].apply(lambda x: f"{x}.npy")
-        existing_embs = [os.path.basename(x) for x in glob(f"{CLIP_VISUAL_EMBS_DIR}/*")]
-        file_filter = ~file_filter.isin(existing_embs)
-        df = df.loc[file_filter]
-        df[COL_NAME_NAME] = df[COL_NAME_NAME].astype(str)
-        df[COL_NAME_DESCRIPTION] = df[COL_NAME_DESCRIPTION].astype(str)
-
-        BATCH_SIZE = 750
-        for batch in tqdm(
-            chunker(df.loc[:, [COL_NAME_ITEM_ID, COL_NAME_NAME, COL_NAME_DESCRIPTION, COL_NAME_IMAGE_FILE]], BATCH_SIZE),
+    file_filter = df[COL_NAME_ITEM_ID].apply(lambda x: f"{x}.npy")
+    existing_embs = [os.path.basename(x) for x in glob(f"{CLIP_VISUAL_EMBS_DIR}/*")]
+    file_filter = ~file_filter.isin(existing_embs)
+    df = df.loc[file_filter]
+    df[COL_NAME_NAME] = df[COL_NAME_NAME].astype(str)
+    df[COL_NAME_DESCRIPTION] = df[COL_NAME_DESCRIPTION].astype(str)
+    BATCH_SIZE = 750
+    for batch in tqdm(
+            chunker(df.loc[:, [COL_NAME_ITEM_ID, COL_NAME_NAME, COL_NAME_DESCRIPTION, COL_NAME_IMAGE_FILE]],
+                    BATCH_SIZE),
             total=int(np.ceil(len(df) / BATCH_SIZE)),
-        ):
-            image_files = batch[COL_NAME_IMAGE_FILE].values
-            embs = clip_encode(image_files)
-            files = [f"{CLIP_VISUAL_EMBS_DIR}/{x[0]}.npy" for x in batch.values]
-            for i in range(len(image_files)):
-                np.save(files[i], embs[i])
+    ):
+        image_files = batch[COL_NAME_IMAGE_FILE].values
+        embs = clip_encode(image_files)
+        files = [f"{CLIP_VISUAL_EMBS_DIR}/{x[0]}.npy" for x in batch.values]
+        for i in range(len(image_files)):
+            np.save(files[i], embs[i])
+
+
+if __name__ == "__main__":
+    download_dataset()
+    train_df = get_dataframe("train")
+    test_df = get_dataframe("test")
+    for df, df_name in zip([train_df, test_df], ["train", "test"]):
+        generate_image_embeddings(df, df_name)
