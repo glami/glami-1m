e8510220b1b4b3a4bd134f6d8aa6df4b07e5e986
Vaclav Kosar <admin@vaclavkosar.com>
2023-02-09 12:02:21 +0100

    generate_image_embeddings optionally all_into_memory_only

--- classification/generate_clip_img_emb.py
+++ classification/generate_clip_img_emb.py
@@ -21,7 +21,7 @@ import clip
 from PIL import Image
 
 
-def generate_image_embeddings(df: pd.DataFrame, df_name: str):
+def generate_image_embeddings(df: pd.DataFrame, all_into_memory_only: bool = False) -> np.ndarray:
     if not os.path.exists(CLIP_VISUAL_EMBS_DIR):
         os.mkdir(CLIP_VISUAL_EMBS_DIR)
 
@@ -41,10 +41,13 @@ def generate_image_embeddings(df: pd.DataFrame, df_name: str):
             image_features = model.encode_image(torch.concat(images, dim=0))
             return image_features.cpu().detach().numpy()
 
-    file_filter = df[COL_NAME_ITEM_ID].apply(lambda x: f"{x}.npy")
-    existing_embs = [os.path.basename(x) for x in glob(f"{CLIP_VISUAL_EMBS_DIR}/*")]
-    file_filter = ~file_filter.isin(existing_embs)
-    df = df.loc[file_filter]
+    if not all_into_memory_only:
+        existing_embs = [os.path.basename(x) for x in glob(f"{CLIP_VISUAL_EMBS_DIR}/*")]
+        file_filter = df[COL_NAME_ITEM_ID].apply(lambda x: f"{x}.npy")
+        file_filter = ~file_filter.isin(existing_embs)
+        df = df.loc[file_filter]
+
+    all_embs = None
     df[COL_NAME_NAME] = df[COL_NAME_NAME].astype(str)
     df[COL_NAME_DESCRIPTION] = df[COL_NAME_DESCRIPTION].astype(str)
     BATCH_SIZE = 750
@@ -55,9 +58,18 @@ def generate_image_embeddings(df: pd.DataFrame, df_name: str):
     ):
         image_files = batch[COL_NAME_IMAGE_FILE].values
         embs = clip_encode(image_files)
-        files = [f"{CLIP_VISUAL_EMBS_DIR}/{x[0]}.npy" for x in batch.values]
-        for i in range(len(image_files)):
-            np.save(files[i], embs[i])
+        if all_embs is None:
+            all_embs = np.array(embs)
+
+        else:
+            all_embs = np.concatenate([all_embs, embs])
+
+        if not all_into_memory_only:
+            files = [f"{CLIP_VISUAL_EMBS_DIR}/{x[0]}.npy" for x in batch.values]
+            for i in range(len(image_files)):
+                np.save(files[i], embs[i])
+
+    return all_embs
 
 
 if __name__ == "__main__":
@@ -65,4 +77,4 @@ if __name__ == "__main__":
     train_df = get_dataframe("train")
     test_df = get_dataframe("test")
     for df, df_name in zip([train_df, test_df], ["train", "test"]):
-        generate_image_embeddings(df, df_name)
+        generate_image_embeddings(df, all_into_memory_only=False)
